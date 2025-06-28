f826ea4c287559b8e99e1ff817b8243707920b0e
alvaro <alvarodevcode@outlook.com>
2025-06-28 17:12:00 -0500

    fix: landing?

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -13,9 +13,8 @@ export default function HomePage() {
   return (
     <main className="container h-screen py-16">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-primary">T3</span> Turbo
-        </h1>
+        <h1 className="text-5xl tracking-tight sm:text-[5rem]">Stackk Biomedical</h1>
+        <p>FHIR & HL7 compliant biomedical data platform + inventory management</p>
 
         <Suspense>
           <AuthShowcase />
